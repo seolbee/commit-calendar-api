0b41286412b6d47d2912e637d09eaec1650cefc5
seolbee <tmf0909@daum.net>
2022-08-09 01:55:59 +0900

    github 연동

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-const request = require('request');
+const koa = require('koa');
+const koaRouter = require('koa-router');
+const views = require('koa-views');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+require('dotenv').config();
+
+const app = new koa();
+const router = new koaRouter();
+
+app.use(views(path.join(__dirname, 'views'), { extends: nunjucks }));
+nunjucks.configure('views', {
+    koa:app
+});
+
+router.get('/', async (ctx) => {
+    await ctx.render('index', {
+        clientId : process.env.GITHUB_CLIENTID,
+        user: process.env.GITHUB_USER,
+        email: process.env.GITHUB_EMAIL
+    });
+});
+
+app.use(router.routes());
+
+app.listen('8090', ()=> {
+    console.log('서버 실행 중');
+});
