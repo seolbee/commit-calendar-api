dacac4b5bfd06349f3192fd491e1561d3ddc8451
seolbee <tmf0909@daum.net>
2022-08-10 01:24:45 +0900

    github 토큰 발행 구현

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
+import { nextTick } from "process";
+
 const koa = require('koa');
 const koaRouter = require('koa-router');
 const views = require('koa-views');
 const path = require('path');
 const nunjucks = require('nunjucks');
+const request = require('request');
 
 require('dotenv').config();
 
 const app = new koa();
 const router = new koaRouter();
 
-app.use(views(path.join(__dirname, 'views'), { extends: nunjucks }));
+const token_obj = {
+    token:'',
+    type:''
+};
+
+app.use(views(path.join(__dirname, 'views'), { map : { html : 'nunjucks' }}));
 nunjucks.configure('views', {
     koa:app
 });
@@ -21,6 +29,42 @@ router.get('/', async (ctx) => {
         email: process.env.GITHUB_EMAIL
     });
 });
+
+router.get('/callback', async (ctx, next) => {
+    let {code} = ctx.query;
+    
+    await request({
+        url: 'https://github.com/login/oauth/access_token',
+        method:'post',
+        headers:{
+            Accept:'application/json'
+        },
+        form : {
+            client_id:process.env.GITHUB_CLIENTID,
+            client_secret:process.env.GITHUB_CLIENTSECRET,
+            code : code,
+            redirect_uri:'http://localhost:8090/callback'
+        }
+    }, async function(err, res, body){
+        try {
+            if(res.statusCode == 200){
+                body = JSON.parse(body);
+                token_obj.token = body.access_token;
+                token_obj.type = body.token_type;
+            }
+        } catch (error) {
+            console.error(err);
+        }
+    });
+
+    await ctx.redirect('/dashboard');
+});
+
+router.get('/dashboard', async (ctx) => {
+    await ctx.render('dashboard', {
+
+    });
+})
 
 app.use(router.routes());
 
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 COMMIT - CALENDAR - API
 
-github에서 push 할 때 commit 한 내역들을 notion 달력에 연동하는 api
+github에서 push 할 때 commit 한 내역들을 notion 달력에 연동하는 api  
 심심해서 만드는 거
+
